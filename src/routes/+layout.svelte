e27cd0c4d0d08cb7957279a420f0d0a039f178b0
benji <benjamin6903@riseup.net>
2026-05-05 21:16:35 +0100

    Initialize project

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<title>shroud.social</title>
+</svelte:head>
+
+{@render children()}
