66d75e0aaa207a54a986b6ef555876bd5c838613
benji <benjamin6903@riseup.net>
2026-05-11 01:21:44 +0100

    new styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,21 +17,48 @@
 </svelte:head>
 
 <main>
-	<header>
-		<nav>
-			<a class="logo" href="/"><Brand variant="mini" style="--brand-size: 48px;"/></a>
-			<div class="social-links">
-				<a class="social-link" href="https://bsky.app/profile/shroud.social"><img src={bluesky} alt="BlueSky"></a>
-				<a class="social-link" href="https://mastodon.social/@shroudsocial"><img src={mastodon} alt="Mastodon"></a>
-				<a class="social-link" href="https://github.com/shroud-org"><img src={github} alt="GitHub"></a>
-			</div>
+	<header class="hstack">
+		<a class="logo" href="/"><Brand variant="mini" style="--brand-size: 48px;"/></a>
+		<div class="social-links hstack">
+			<a class="social-link" href="https://bsky.app/profile/shroud.social"><img src={bluesky} alt="BlueSky"></a>
+			<a class="social-link" href="https://mastodon.social/@shroudsocial"><img src={mastodon} alt="Mastodon"></a>
+			<a class="social-link" href="https://github.com/shroud-org"><img src={github} alt="GitHub"></a>
+		</div>
+		<nav class="hstack">
+			<ul>
+				<li class="nav-element">
+					<a href="#who">Who</a>
+				</li>
+				<li class="nav-element">
+					<a href="#what">What</a>
+				</li>
+				<li class="nav-element">
+					<a href="#where">Where</a>
+				</li>
+				<li class="nav-element">
+					<a href="#when">When</a>
+				</li>
+				<li class="nav-element">
+					<a href="#why">Why</a>
+				</li>
+			</ul>
 		</nav>
 	</header>
-	<div class="content">
+	<main class="content vstack">
 		{@render children()}
-	</div>
-	<footer>
-		<div class="socials"></div>
+	</main>
+	<footer class="vstack">
+		<div class="info hstack">
+			<div class="contacts">
+				<h2>Contacts us:</h2>
+				<p>support@shroud.social</p>
+				<p>support@shroud.org.uk</p>
+			</div>
+			<div class="company">
+				<h2>About:</h2>
+				<p>Shroud Labs</p>
+			</div>
+		</div>
 		<div class="brand">
 			<Brand style="--brand-size: auto; --brand-width: 100%;" />
 			<img src={clouds} alt="clouds" />
@@ -40,27 +67,41 @@
 </main>
 
 <style>
-	main {
+	.vstack {
 		display: flex;
 		flex-direction: column;
+	}
+
+	.hstack {
+		display: flex;
+		flex-direction: row;
+	}
+
+	main {
 		background: #14002D;
 		background: linear-gradient(190deg, rgba(20, 0, 45, 1) 21%, rgba(24, 99, 165, 1) 100%);
 		color: #fff;
 	}
 
-	nav {
+	header {
+		position: fixed;
+		z-index: 100;
+		top: 0;
 		padding: 32px;
-		display: flex;
-		flex-direction: row;
 		align-items: center;
+		gap: 32px;
+		width: 100vw;
+		box-sizing: border-box;
+	}
+
+	nav {
+		display: flex;
+		margin-left: auto;
 		gap: 20px;
-		width: 100%;
 	}
 
 	.social-links {
-		display: flex;
-		gap: 25px;
-		padding-left: 15px;
+		gap: 24px;
 		align-items: center;
 	}
 
@@ -73,41 +114,60 @@
 		opacity: 0.6;
 	}
 
-	ul {
-		list-style: none;
-		margin: 0;
-		padding: 0;
-		display: flex;
-		width: 100%;
-		height: 50px;
-		justify-content: center;
-		align-items: center;
-		gap: 1rem;
-	}
-
 	a {
 		text-decoration: none;
+		color: #ffffff;
 	}
 
 	a:visited {
 		color: #fff;
 	}
 
+	ul {
+		list-style: none;
+		padding: 0;
+		margin: 0;
+		display: flex;
+		flex-direction: row;
+		gap: 20px;
+	}
+
+	li.nav-element {
+		font-weight: bold;
+		font-size: 18px;
+	}
+
+	li.nav-element:hover {
+		opacity: 0.8;
+	}
+
+	li.nav-element:active {
+		opacity: 0.6;
+	}
+
 	.content {
-		flex: 1;
-		width: 100vw;
+		width: 100%;
 	}
 
 	footer {
+		position: relative;
+		padding: 20px 0 1% 0;
+		gap: 20px;
+	}
+
+	.info {
+		padding: 64px;
 		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding-bottom: 1%;
+		gap: 32px;
 	}
 
 	.brand {
-		width: 95%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 100%;
+		padding: 0 2% 0 2%;
+		box-sizing: border-box;
 		opacity: 87%;
 	}
 
@@ -116,7 +176,6 @@
 		bottom: 0;
 		left: 0;
 		width: 100vw;
-		height: auto;
 		opacity: 20%;
 		z-index: -1;
 	}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,47 +2,121 @@
     import { Quote } from "@lucide/svelte";
 </script>
 
-<div class="content">
-    <div class="newsletter">
-        <h1>We're building the next generation of group communication.</h1>
-        <h3>Subscribe to the newsletter for updates:</h3>
-        <form action="https://buttondown.com/api/emails/embed-subscribe/shroudsocial"
+<div class="container">
+    <div class="hero">
+        <div>
+            <h1>shroud.social is coming soon.</h1>
+            <h2>A new home for your community.</h2>
+        </div>
+        <form
+                action="https://buttondown.com/api/emails/embed-subscribe/shroudsocial"
                 method="post"
-                class="embeddable-buttondown-form" >
-            <label for="bd-email">Enter your email:</label>
-            <input type="email" name="email" id="bd-email" />
+                class="embeddable-buttondown-form"
+        >
+            <h3>Sign up for the newsletter:</h3>
+            <input type="email" name="email" id="bd-email" placeholder="Enter your email" />
             <input type="submit" value="Subscribe" />
             <p>
-                <a href="https://buttondown.com/refer/shroudsocial" target="_blank">
+                <a href="https://buttondown.com/refer/shroudsocial" target="_blank" class="buttondown-link">
                     Powered by Buttondown.
                 </a>
             </p>
         </form>
     </div>
+    <div class="content">
+        <div class="section" id="who">
+            <h1>Who?</h1>
+            <p>shroud.labs is an upcoming social enterprise dedicated to building open source, decentralized and privacy-optimised services.</p>
+            <p>All code is and will be 100% human written.</p>
+        </div>
+        <div class="section" id="what">
+            <h1>What?</h1>
+            <p>shroud.social is a decentralized and private alternative to existing community messaging services.</p>
+        </div>
+        <div class="section" id="where">
+            <h1>Where?</h1>
+            <p>shroud.social is a federated service, which means instead of being forced to trust one company with your data, you have the option to keep it to yourself or a company you trust, whilst still having access to your communities.</p>
+            <p>A flagship instance will be run by us, anyone else is welcome to run their own instance for themselves, their friends, for anyone.</p>
+        </div>
+        <div class="section" id="when">
+            <h1>When?</h1>
+            <p>shroud.social is currently in development, there is no current planned release date but you can sign up for our <a href="https://buttondown.com/shroudsocial">newsletter</a> for news updates.</p>
+        </div>
+        <div class="section" id="why">
+            <h1>Why?</h1>
+            <p>The internet should be free, open, decentralized and private. So far, we have been failed. Large corporations run too many services which are required for most people. We want to change that, starting with shroud.social</p>
+            <p>Read the shroud.labs mission statement <a href="https://buttondown.com/shroudsocial/archive/shroudlabs-mission-statement/">here</a></p>
+        </div>
+    </div>
 </div>
 
 <style>
+    .container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .hero {
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
+        gap: 64px;
+        justify-content: center;
+    }
+
+    .hero h1 {
+        font-size: 64px;
+        font-family: 'ZT Nature', sans-serif;
+        font-style: normal;
+    }
+
     .content {
         display: flex;
         flex-direction: column;
-        justify-content: center;
         align-items: center;
-        gap: 25px;
+        flex: 1;
+        width: 100%;
+        background: rgb(255 255 255 / 0.7);
+        color: #000000;
+        gap: 20px;
+        box-sizing: border-box;
+        padding: 50px;
     }
 
-    .newsletter {
+    .section {
         display: flex;
         flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        background: radial-gradient(circle at 50% 20%, rgb(222 255 255 / 0.11) 0%, rgb(222 255 255 / 0.11) 100%);
-        width: 100%;
-        padding: 50px;
-        backdrop-filter: blur(50px);
-        border: 1px solid rgba(255, 255, 255, 0.1);
+        justify-content: left;
+        width: 70vh;
     }
 
-    h1, h3, form {
-        text-align: center;
+    input[type="email"] {
+        padding: 12px;
+        margin: 8px 0;
+        display: inline-block;
+        border: 1px solid #ccc;
+        border-radius: 25px;
+        box-sizing: border-box;
+    }
+
+    input[type=submit] {
+        background-color: #4CAF50;
+        color: white;
+        padding: 14px;
+        margin: 8px 0;
+        border: none;
+        border-radius: 25px;
+        cursor: pointer;
+    }
+
+    input[type=submit]:hover {
+        background-color: #45a049;
+    }
+
+    a.buttondown-link {
+        text-decoration: none;
+        color: rgb(255 255 255 / 0.4);
     }
 </style>
