e14af58e43bda94cfbbe38210327cb667e70eed6
benji <benjamin6903@riseup.net>
2026-05-12 21:31:37 +0100

    transition to scss

--- src/routes/subscribed/+page.svelte
+++ src/routes/subscribed/+page.svelte
@@ -1,24 +1,24 @@
 <script>
 </script>
 
-<div class="content">
+<div class="container">
     <div class="thanks">
         <h1>Thanks for subscribing!</h1>
         <p>Check your emails to confirm your subscription.</p>
     </div>
 </div>
 
-<style>
+<style lang="scss">
     .thanks {
         margin-top: 10vh;
         text-align: center;
-    }
 
-    h1 {
-        font-size: clamp(3rem, 5vw, 7rem);
-    }
+        h1 {
+            font-size: clamp(3rem, 5vw, 7rem);
+        }
 
-    p {
-        font-size: clamp(1rem, 2.25vw, 4rem);
+        p {
+            font-size: clamp(1rem, 2.25vw, 4rem);
+        }
     }
 </style>
