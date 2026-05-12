ef8110b62c1fe0765eaaa3b5af3d6f938d3d29ce
benji <benjamin6903@riseup.net>
2026-05-12 21:31:22 +0100

    add reactivity and transition to scss

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,10 +4,25 @@
 	import bluesky from '$lib/assets/bluesky.svg';
 	import mastodon from '$lib/assets/mastodon.svg';
 	import github from '$lib/assets/github.svg';
+	import { Menu } from '@lucide/svelte';
+	import { X } from '@lucide/svelte';
 	import '$lib/assets/style.css';
 	import Brand from '$lib/assets/components/Brand.svelte'
 
 	let { children } = $props();
+
+	let scrollY = $state(0);
+	let innerHeight = $state(0);
+	let innerWidth = $state(0);
+
+	let isScrolled = $derived(scrollY > innerHeight - 100);
+
+	let navOpened = $state(false);
+	function toggleNav() {
+		navOpened = !navOpened;
+	}
+
+	let showNavBackground = $derived(isScrolled && innerWidth > 700);
 </script>
 
 <svelte:head>
@@ -16,39 +31,52 @@
 	<meta name="description" content="shroud.social, the next generation of private, open-source and decentralized group communication." />
 </svelte:head>
 
-<main>
-	<header class="hstack">
-		<a class="logo" href="/"><Brand variant="mini" style="--brand-size: 48px;"/></a>
-		<div class="social-links hstack">
+<svelte:window bind:scrollY={scrollY} bind:innerHeight={innerHeight} bind:innerWidth={innerWidth} />
+
+<div class="page">
+	<header>
+		<div class="logo header-component" class:scrolled={isScrolled}>
+			<a href="/"><Brand variant="mini" style="--brand-size: 48px;"/></a>
+		</div>
+		<div class="social-links header-component" class:scrolled={isScrolled}>
 			<a class="social-link" href="https://bsky.app/profile/shroud.social"><img src={bluesky} alt="BlueSky"></a>
 			<a class="social-link" href="https://mastodon.social/@shroudsocial"><img src={mastodon} alt="Mastodon"></a>
 			<a class="social-link" href="https://github.com/shroud-org"><img src={github} alt="GitHub"></a>
 		</div>
-		<nav class="hstack">
-			<ul>
-				<li class="nav-element">
-					<a href="#who">Who</a>
-				</li>
-				<li class="nav-element">
-					<a href="#what">What</a>
-				</li>
-				<li class="nav-element">
-					<a href="#where">Where</a>
-				</li>
-				<li class="nav-element">
-					<a href="#when">When</a>
-				</li>
-				<li class="nav-element">
-					<a href="#why">Why</a>
-				</li>
-			</ul>
-		</nav>
+		<div class="navigation header-component" class:scrolled={showNavBackground}>
+			<nav class:open={navOpened}>
+				<ul>
+					<li class="nav-element">
+						<a href="#who" onclick={toggleNav}>Who</a>
+					</li>
+					<li class="nav-element">
+						<a href="#what" onclick={toggleNav}>What</a>
+					</li>
+					<li class="nav-element">
+						<a href="#where" onclick={toggleNav}>Where</a>
+					</li>
+					<li class="nav-element">
+						<a href="#when" onclick={toggleNav}>When</a>
+					</li>
+					<li class="nav-element">
+						<a href="#why" onclick={toggleNav}>Why</a>
+					</li>
+				</ul>
+			</nav>
+			<button id="nav-menu" onclick={toggleNav}>
+				{#if navOpened}
+					<X />
+				{:else}
+					<Menu />
+				{/if}
+			</button>
+		</div>
 	</header>
-	<main class="content vstack">
+	<main>
 		{@render children()}
 	</main>
-	<footer class="vstack">
-		<div class="info hstack">
+	<footer>
+		<div class="info">
 			<div class="contacts">
 				<h2>Contacts us:</h2>
 				<p>support@shroud.social</p>
@@ -64,119 +92,237 @@
 			<img src={clouds} alt="clouds" />
 		</div>
 	</footer>
-</main>
+</div>
 
-<style>
-	.vstack {
-		display: flex;
-		flex-direction: column;
-	}
+<style lang="scss">
+	$small-screen: 700px;
+	$medium-screen: 900px;
+	$large-screen: 1200px;
 
-	.hstack {
-		display: flex;
-		flex-direction: row;
-	}
-
-	main {
+	.page {
 		background: #14002D;
 		background: linear-gradient(190deg, rgba(20, 0, 45, 1) 21%, rgba(24, 99, 165, 1) 100%);
 		color: #fff;
 	}
 
 	header {
+		display: flex;
+		flex-direction: row;
 		position: fixed;
 		z-index: 100;
 		top: 0;
-		padding: 32px;
+		padding: 16px;
 		align-items: center;
-		gap: 32px;
+		gap: 12px;
 		width: 100vw;
 		box-sizing: border-box;
+
+		.header-component {
+			padding: 16px;
+			border: 1px solid rgb(255 255 255 / 0);
+			border-radius: 50px;
+			transition: background 0.3s ease, border 0.3s ease;
+		}
+
+		.scrolled {
+			background: rgb(0 0 0 / 0.34);
+			backdrop-filter: blur(10px);
+			border: 1px solid rgb(255 255 255 / 0.2);
+
+			a {
+				&:hover {
+					opacity: 1;
+					background: rgb(0 0 0 / 0.4);
+					backdrop-filter: blur(10px);
+				}
+			}
+		}
+
+		a {
+			text-decoration: none;
+			color: #ffffff;
+
+			&:hover {
+				opacity: 0.8;
+			}
+
+			&:visited {
+				color: #ffffff;
+			}
+		}
+
+		.logo {
+			display: flex;
+			height: 48px;
+			width: 48px;
+		}
+
+		.social-links {
+			display: flex;
+			flex-direction: row;
+			gap: 0;
+			justify-content: center;
+			align-items: center;
+			padding: 4px;
+
+			.social-link {
+				line-height: 0;
+				display: inline-flex;
+				align-items: center;
+				padding: 12px;
+
+				img {
+					width: 32px;
+					height: 32px;
+					object-fit: contain;
+					opacity: 0.8;
+				}
+			}
+		}
+
+		.navigation {
+			margin-left: auto;
+
+			@media screen and (max-width: $small-screen) {
+				nav {
+					display: none;
+					flex-direction: column;
+					position: fixed;
+					z-index: -1;
+					top: 8px;
+					left: 50%;
+					transform: translateX(-50%);
+					width: 95%;
+					background: rgb(0 0 0 / 0.4);
+					backdrop-filter: blur(10px);
+					border: 1px solid rgb(255 255 255 / 0.4);
+					border-radius: 48px;
+
+					transition: display 0.3s ease;
+
+					&.open {
+						display: flex;
+					}
+
+					ul {
+						display: flex;
+						flex-direction: column;
+						justify-content: center;
+						align-items: center;
+						margin: 0;
+						padding: 96px 0 16px 0;
+						list-style: none;
+						gap: 48px;
+
+						li.nav-element {
+							font-weight: bold;
+							font-size: 48px;
+
+							&:hover {
+								opacity: 0.8;
+							}
+
+							&:active {
+								opacity: 0.6;
+							}
+
+							a {
+								padding: 12px;
+							}
+						}
+					}
+				}
+
+				button {
+					display: inline-block;
+					justify-content: center;
+					align-items: center;
+					height: 48px;
+					width: 48px;
+					padding: 4px;
+					border: none;
+					border-radius: 25px;
+					background-color: rgb(255 255 255 / 0.87);
+					color: #000000;
+					line-height: 0;
+				}
+			}
+
+			@media screen and (min-width: $small-screen) {
+				nav {
+					display: flex;
+					flex-direction: row;
+
+					ul {
+						display: flex;
+						flex-direction: row;
+						list-style: none;
+						padding: 0;
+						margin: 0;
+
+						li.nav-element {
+							font-weight: bold;
+							font-size: 18px;
+
+							&:hover {
+								opacity: 0.8;
+							}
+
+							&:active {
+								opacity: 0.6;
+							}
+
+							a {
+								padding: 12px;
+							}
+						}
+					}
+				}
+
+				button {
+					display: none;
+				}
+			}
+		}
 	}
 
-	nav {
+	main {
 		display: flex;
-		margin-left: auto;
-		gap: 20px;
-	}
-
-	.social-links {
-		gap: 24px;
-		align-items: center;
-	}
-
-	.social-link img {
-		width: 32px;
-		opacity: 0.8;
-	}
-
-	.social-link:hover {
-		opacity: 0.6;
-	}
-
-	a {
-		text-decoration: none;
-		color: #ffffff;
-	}
-
-	a:visited {
-		color: #fff;
-	}
-
-	ul {
-		list-style: none;
-		padding: 0;
-		margin: 0;
-		display: flex;
-		flex-direction: row;
-		gap: 20px;
-	}
-
-	li.nav-element {
-		font-weight: bold;
-		font-size: 18px;
-	}
-
-	li.nav-element:hover {
-		opacity: 0.8;
-	}
-
-	li.nav-element:active {
-		opacity: 0.6;
-	}
-
-	.content {
+		flex-direction: column;
 		width: 100%;
 	}
 
 	footer {
+		display: flex;
+		flex-direction: column;
 		position: relative;
 		padding: 20px 0 1% 0;
 		gap: 20px;
-	}
 
-	.info {
-		padding: 64px;
-		display: flex;
-		gap: 32px;
-	}
+		.info {
+			display: flex;
+			flex-direction: row;
+			padding: 64px;
+			gap: 32px;
+		}
 
-	.brand {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		width: 100%;
-		padding: 0 2% 0 2%;
-		box-sizing: border-box;
-		opacity: 87%;
-	}
+		.brand {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			width: 100%;
+			padding: 0 2% 0 2%;
+			box-sizing: border-box;
+			opacity: 87%;
 
-	.brand img {
-		position: absolute;
-		bottom: 0;
-		left: 0;
-		width: 100vw;
-		opacity: 20%;
-		z-index: -1;
+			img {
+				position: absolute;
+				bottom: 0;
+				left: 0;
+				width: 100vw;
+				opacity: 20%;
+				z-index: -1;
+			}
+		}
 	}
 </style>
